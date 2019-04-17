630bd8d057559a7026100244d45b1bbefd1c1eb5
yddmat <yddmat@gmail.com>
2019-04-18 00:23:30 +0300

    RPC server added

---
+++ app/api/http/jrpc/server.go
@@ -0,0 +1,29 @@
+package jrpc
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+
+	"github.com/yddmat/currency-converter/app/types"
+)
+
+type Server struct {
+	Converter types.Converter
+	Port      string
+}
+
+func (s *Server) Start() error {
+	jrpc := rpc.NewServer()
+	jrpc.RegisterCodec(json.NewCodec(), "application/json")
+	if err := jrpc.RegisterService(&Converter{Converter: s.Converter}, ""); err != nil {
+		return err
+	}
+
+	router := chi.NewRouter()
+	router.Handle("/", jrpc)
+	return http.ListenAndServe(fmt.Sprintf(":%s", s.Port), router)
+}
--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"time"
 
-	"github.com/yddmat/currency-converter/app/api/http"
+	"github.com/yddmat/currency-converter/app/api/http/jrpc"
+
+	"github.com/yddmat/currency-converter/app/api/http/rest"
+
+	"github.com/tarent/logrus"
 	"github.com/yddmat/currency-converter/app/converter"
 	"github.com/yddmat/currency-converter/app/provider"
 	"github.com/yddmat/currency-converter/app/types"
-	"github.com/tarent/logrus"
 )
 
 func main() {
@@ -17,8 +20,14 @@ func main() {
 	}
 
 	converter := converter.NewConverter(provider.NewExchangeRatesIOProvider(), bases, time.Hour)
-	server := http.Server{Converter: converter, Stat: converter}
+	server := rest.Server{Converter: converter, Stat: converter, Port: "8080"}
 
-	logrus.Info("Starting server")
-	logrus.WithError(server.Start()).Fatal("Server can't start")
+	go func() {
+		logrus.Info("Starting rest server")
+		logrus.WithError(server.Start()).Fatal("Rest server can't start")
+	}()
+
+	logrus.Info("Starting rpc server")
+	rpc := jrpc.Server{Converter: converter, Port: "4444"}
+	logrus.WithError(rpc.Start()).Fatal("RPC server can't start")
 }
